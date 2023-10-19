1bcc2ffc2569a7b9bebda4fd595866427ebf7c7b
Idedav <xdavide.correa@gmail.com>
2023-10-19 17:41:57 +0200

    [add] special moments

--- src/components/partials/SpecialMoments.vue
+++ src/components/partials/SpecialMoments.vue
@@ -5,9 +5,90 @@ export default {
 </script>
 
 <template>
-  <h1>SPECIAL MOMENTS</h1>
+  <div class="container-fluid d-flex flex-column">
+
+    <div class="text-container d-flex flex-column align-items-center my-4">
+      <span>CORPORATE & WEDDINGS</span>
+      <h2>Baking Special Moments</h2>
+    </div>
+
+    <div class="images-container d-flex justify-content-between">
+
+      <div class="corporate">
+
+          <div class="text-container flex-column align-items-center">
+            <span>INTERNATIONAL BAKERY</span>
+            <h2>CORPORATE EVENTS</h2>
+            <button class="btn-purple">Explore more</button>
+          </div>
+
+      </div>
+
+      <div class="weddings">
+
+          <div class="text-container flex-column align-items-center">
+            <span>PRIVATE DINING HALL</span>
+            <h2>Wedding & Parties</h2>
+            <button class="btn-purple">Explore more</button>
+          </div>
+
+      </div>
+
+    </div>
+
+  </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
+
+.text-container{
+  h2{
+    margin: 20px 0px;
+  }
+}
+
+.images-container{
+  width: 100%;
+
+  .corporate,
+  .weddings{
+    width: 48%;
+    height: 900px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .text-container{
+    display: none;
+    span{
+      color: #D2D3D4;
+    }
+    h2{
+      color: white;
+    }
+  }
+  .corporate{
+    background-image: url(../../assets/img/corporate-bg.jpg);
+    background-size: cover;
+    &:hover{
+      background-image: url(../../assets/img/corporate-hover-bg.jpg);
+    }
+    &:hover .text-container{
+      display: flex;
+    }
+  }
+
+  .weddings{
+    background-image: url(../../assets/img/wedding-bg.jpg);
+    background-size: cover;
+    &:hover{
+      background-image: url(../../assets/img/wedding-hover-bg.jpg);
+    }
+    &:hover .text-container{
+      display: flex;
+    }
+  }
+}
 
 </style>
