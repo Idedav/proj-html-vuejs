d4630a72ca82388db43c18739bdca81445b49c42
Idedav <xdavide.correa@gmail.com>
2023-10-20 15:44:12 +0200

    [add] locations

--- src/components/partials/main-sections/Locations.vue
+++ src/components/partials/main-sections/Locations.vue
@@ -5,9 +5,118 @@ export default {
 </script>
 
 <template>
-  <h1>LOCATION</h1>
+
+  <div class="container-fluid my-5">
+
+    <div class="text-container text-center">
+      <span>LOCATIONS</span>
+      <h2>Visit our Bakeries</h2>
+    </div>
+
+    <div class="row my-5">
+
+      <div class="col d-flex">
+
+        <div class="location new-york">
+
+          <div class="image">
+            <img src="../../../assets/img/new-york-bk.jpg" alt="">
+          </div>
+          
+          <div class="text-container d-flex flex-column justify-content-center align-items-center text-center">
+            
+            <h2>New York</h2>
+
+            <div class="references d-flex justify-content-between">
+
+              <div>
+                <span>CALL US</span>
+                <p>1.800.458.556</p>
+              </div>
+
+              <div>
+                <span>OPEN ALL WEEK</span>
+                <p>9:00 AM - 6:00 PM</p>
+              </div>
+
+              <button class="btn-white">View Map</button>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="location london">
+
+          <div class="image">
+            <img src="../../../assets/img/london-bk.jpg" alt="">
+          </div>
+
+          <div class="text-container d-flex flex-column justify-content-center align-items-center text-center">
+            
+            <h2>London</h2>
+
+            <div class="references d-flex justify-content-between">
+
+              <div>
+                <span>CALL US</span>
+                <p>1.800.458.556</p>
+              </div>
+
+              <div>
+                <span>OPEN ALL WEEK</span>
+                <p>9:00 AM - 6:00 PM</p>
+              </div>
+
+              <button class="btn-white">View Map</button>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
 </template>
 
-<style>
+<style lang="scss" scoped>
+
+.col{
+  height: 800px;
+  .new-york{
+    background-color: #F1F9FB;
+  }
+  .london{
+    background-color: #FAF3E3;
+  }
+  .location{
+    width: 50%;
+    height: 100%;
+    .image{
+      width: 100%;
+      height: 75%;
+      img{
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .text-container{
+      height: 25%;
+      h2{
+        margin-bottom: 30px;
+      }
+      .references{
+        height: 20%;
+        width: 90%;
+      }
+    }
+  }
+}
 
 </style>
